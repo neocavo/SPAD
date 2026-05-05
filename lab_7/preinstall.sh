5d6de3a516b39cdae6193b402b7e0b2d4d4e0d1d
neocavo <kocharov2007@gmail.com>
2026-05-05 14:04:50 +0300

    Update preinstall.sh

--- lab_7/preinstall.sh
+++ lab_7/preinstall.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 sudo apt-get update --fix-missing
 sudo apt-get install -y libopencv-dev cmake g++
+
+# Завантажуємо файли нейронної мережі ResNet-10
+wget -q --show-progress -O deploy.prototxt \
+    https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
+
+wget -q --show-progress -O res10_300x300_ssd_iter_140000.caffemodel \
+    https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel
